c4e9b74a4fb2f9502ca257a78c1aa7b8683d5a5a
Nirav Adatiya <niravjadatiya@gmail.com>
2018-07-29 22:23:17 +0530

    updated database js
    secret key added

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {database:
-    'mongodb://jacqann:XMLhtml4.01@ds259111.mlab.com:59111/irish4fun'} 
+    'mongodb://jacqann:XMLhtml4.01@ds259111.mlab.com:59111/irish4fun',
+    secret: 'your secret'} 
 } else
 module.exports = {
   database: 'mongodb://localhost:27017/irish4fun',
